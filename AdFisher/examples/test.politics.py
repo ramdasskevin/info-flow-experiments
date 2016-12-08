0e1eb39118a2185b8111a228af1fa2f6b078d3d1
Federico Ponte <fedep3@gmail.com>
2016-12-07 21:42:41 -0800

    Adding log file of experiment

--- AdFisher/examples/test.politics.py
+++ AdFisher/examples/test.politics.py
@@ -15,7 +15,7 @@ def make_browser(unit_id, treatment_id):
     return b
 
 web.pre_experiment.alexa.collect_sites(make_browser, num_sites=15, output_file=site_file_politics,
-    alexa_link="http://www.alexa.com/topsites/category/Top/Society/Politics/Nationalism")
+    alexa_link="http://www.alexa.com/topsites/category/Top/Society/Politics/Campaigns_and_Elections")
 
 
 # Control Group treatment
@@ -56,6 +56,6 @@ def test_stat(observed_values, unit_assignments):
 adfisher.do_experiment(make_unit=make_browser, treatments=[control_treatment, exp_treatment], 
                         measurement=measurement, end_unit=cleanup_browser,
                         load_results=load_results, test_stat=test_stat, ml_analysis=True, 
-                        num_blocks=20, num_units=2, timeout=2000,
+                        num_blocks=20, num_units=4, timeout=2000,
                         log_file=log_file,
                         treatment_names=["control ()", "experimental (politics)"])
